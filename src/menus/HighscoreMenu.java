bc297c5c55241e831f71e44441c5d580cd21d5ac
Fredrik Gerdin Börjesson <frebo147@student.liu.se>
2017-02-24 14:44:30 +0100

    missade en

---
+++ src/menus/HighscoreMenu.java
@@ -0,0 +1,62 @@
+package menus;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import javax.swing.JComponent;
+
+import main.Highscore;
+
+public class HighscoreMenu extends JComponent {
+	private static final long serialVersionUID = 1L;
+	
+	private Highscore highscore;
+	
+	public HighscoreMenu() {
+		openHighscore();
+	}
+
+	private void openHighscore() {
+		File saveFile = new File("save.flapario");
+		if (saveFile.exists() && !saveFile.isDirectory()) {
+			try {
+				ObjectInputStream in = new ObjectInputStream(new FileInputStream(
+						saveFile));
+				highscore = (Highscore) in.readObject();
+				in.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		} else {
+			try {
+				ObjectOutputStream newSaveFile = new ObjectOutputStream(
+						new FileOutputStream(saveFile));
+				newSaveFile.writeObject(highscore);
+				newSaveFile.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
