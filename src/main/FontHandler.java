ac397310d192022888551b4ff924e18ee1d1071c
Fredrik Gerdin Börjesson <frebo147@student.liu.se>
2017-03-03 14:55:16 +0100

    Denna hanterar fonten

---
+++ src/main/FontHandler.java
@@ -0,0 +1,22 @@
+package main;
+
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.GraphicsEnvironment;
+import java.io.File;
+import java.io.IOException;
+
+public class FontHandler {
+	private final String pathBoxyBoldFont = "./src/assets/font/Boxy_Bold.ttf";
+
+	public FontHandler() {
+		try {
+			GraphicsEnvironment ge = GraphicsEnvironment
+					.getLocalGraphicsEnvironment();
+			ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File(
+					pathBoxyBoldFont)));
+		} catch (IOException | FontFormatException e) {
+			e.printStackTrace();
+		}
+	}
+}
